d74dc380edf704e331870852e7121c475add277f
Mike Boers <westernx@mikeboers.com>
2015-02-18 11:11:32 -0800

    Make our own testing tarballs on the fly

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,12 +1,15 @@
 from cStringIO import StringIO
 import atexit
+import fnmatch
 import os
 import random
+import re
 import shutil
 import SimpleHTTPServer
 import SocketServer
 import subprocess
 import sys
+import tarfile
 import urllib2
 
 from unittest import TestCase
@@ -31,19 +34,49 @@ def vee(args, environ=None):
 class MockHTTPHandler(urllib2.HTTPHandler):
 
     def http_open(self, req):
-        if req.get_host() == 'assets.vee.mock':
-            asset_path = os.path.join(assets_dir, req.get_selector().strip('/'))
-            if os.path.exists(asset_path):
-                res = urllib2.addinfourl(open(asset_path), 'HEADERS', req.get_full_url())
-                res.code = 200
-                res.msg = 'OK'
-                return res
-            else:
-                res = urllib2.addinfourl(StringIO('404 NOT FOUND'), 'HEADERS', req.get_full_url())
-                res.code = 404
-                res.msg = 'NOT FOUND'
-                return res
-        return urllib2.HTTPHandler.http_open(self, req)
+
+        if req.get_host() != 'assets.vee.mock':
+            return urllib2.HTTPHandler.http_open(self, req)
+
+        # Files which exist.
+        asset_path = os.path.join(assets_dir, req.get_selector().strip('/'))
+        if os.path.exists(asset_path):
+            res = urllib2.addinfourl(open(asset_path), 'HEADERS', req.get_full_url())
+            res.code = 200
+            res.msg = 'OK'
+            return res
+
+        # Create tarballs on the fly.
+        asset_base, ext = os.path.splitext(asset_path)
+        if os.path.exists(asset_base) and ext == '.tgz':
+
+            fh = StringIO()
+            tgz = tarfile.open(fileobj=fh, mode='w:gz')
+
+            ignore_path = os.path.join(asset_base, 'mockignore')
+            if os.path.exists(ignore_path):
+                patterns = [x.strip() for x in open(ignore_path)] + ['mockignore']
+                pattern = re.compile('|'.join(fnmatch.translate(x) for x in patterns if x))
+
+            for dir_path, dir_names, file_names in os.walk(asset_base):
+                for file_name in file_names:
+                    if pattern.match(file_name):
+                        continue
+                    file_path = os.path.join(dir_path, file_name)
+                    tgz.add(file_path, os.path.relpath(file_path, asset_base))
+
+            tgz.close()
+            fh.seek(0)
+
+            res = urllib2.addinfourl(fh, 'HEADERS', req.get_full_url())
+            res.code = 200
+            res.msg = 'OK'
+            return res
+
+        res = urllib2.addinfourl(StringIO('404 NOT FOUND'), 'HEADERS', req.get_full_url())
+        res.code = 404
+        res.msg = 'NOT FOUND'
+        return res
 
 urllib2.install_opener(urllib2.build_opener(MockHTTPHandler))
 
--- tests/test_http.py
+++ tests/test_http.py
@@ -4,13 +4,13 @@ from . import *
 class TestHttpManager(TestCase):
 
     def test_foobar_step1(self):
-        vee(['install', asset_url('libfoo-1.0.0.tgz'),
+        vee(['install', asset_url('libfoo.tgz'),
             '--install-name', 'libfoo/1.0.0',
         ])
         self.assertTrue(os.path.exists(sandbox('installs/libfoo/1.0.0/lib/libfoo.so')))
 
     def test_foobar_step2(self):
-        vee(['install', asset_url('bar-1.0.0.tgz'),
+        vee(['install', asset_url('bar.tgz'),
             '--install-name', 'bar/1.0.0',
             '--environ', 'CFLAGS=-I$VEE/installs/libfoo/1.0.0/include,LDFLAGS=-L$VEE/installs/libfoo/1.0.0/lib',
         ])
