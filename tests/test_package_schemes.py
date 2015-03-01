f4e926621fa507cf8c60884f9c691436b5cc2424
Mike Boers <github@mikeboers.com>
2015-02-28 17:36:53 -0800

    Mock Python wheels

--- tests/test_package_schemes.py
+++ tests/test_package_schemes.py
@@ -27,9 +27,25 @@ class TestPackageSchemes(TestCase):
 
     def test_py_src(self):
         self.assert_echo('py_src', call=False)
+        # TODO: arbitrary data.
+        # TODO: both scripts and console_scripts entrypoints.
 
     def test_py_bdist(self):
         pkg = MockPackage('scheme_py_egg', 'py_bdist')
         pkg.render_commit()
         vee(['install', sandbox('packages/scheme_py_egg'), '--install-name', 'scheme_py_egg/1.0.0'])
         self.assertExists(sandbox('vee/installs/scheme_py_egg/1.0.0/lib/python2.7/site-packages/scheme_py_egg/__init__.py'))
+        # TODO: arbitrary data.
+        # TODO: scripts and console_scripts entrypoints.
+        # self.assertExists(sandbox('vee/installs/scheme_py_egg/1.0.0/bin/scheme_py_egg'))
+        # self.assertExists(sandbox('vee/installs/scheme_py_egg/1.0.0/bin/scheme_py_egg-ep'))
+
+    def test_py_wheel(self):
+        pkg = MockPackage('scheme_py_whl', 'py_wheel')
+        pkg.render_commit()
+        vee(['install', sandbox('packages/scheme_py_whl'), '--install-name', 'scheme_py_whl/1.0.0'])
+        self.assertExists(sandbox('vee/installs/scheme_py_whl/1.0.0/lib/python2.7/site-packages/scheme_py_whl/__init__.py'))
+        # TODO: arbitrary data.
+        # TODO: scripts and console_scripts entrypoints:
+        # self.assertExists(sandbox('vee/installs/scheme_py_whl/1.0.0/bin/scheme_py_whl'))
+        # self.assertExists(sandbox('vee/installs/scheme_py_whl/1.0.0/bin/scheme_py_whl-ep'))
