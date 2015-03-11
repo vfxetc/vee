15b0ab1a3d3dd3f05e28afb5b1fa09d7925c3a2f
Mike Boers <github@mikeboers.com>
2015-03-11 10:26:33 -0700

    Test the installer

---
+++ tests/test_installer.py
@@ -0,0 +1,21 @@
+from . import *
+
+
+class TestInstaller(TestCase):
+
+    def test_basic_install(self):
+
+        root = self.sandbox()
+        os.makedirs(root)
+
+        subprocess.check_call([
+            'python',
+            os.path.abspath(os.path.join(__file__, '..', '..', 'install_vee.py')),
+            '--prefix', root,
+            '--yes',
+            '--url', os.path.abspath(os.path.join(__file__, '..', '..')),
+            '--no-bashrc',
+        ])
+
+        self.assertExists(os.path.join(root, 'src', 'bin', 'vee'))
+
