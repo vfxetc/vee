b0283c1ef1666cf4df108d218a9f34588caf0a28
Mike Boers <westernx@mikeboers.com>
2015-03-13 15:43:13 -0700

    `vee git --stree` to open SourceTree

--- vee/commands/git.py
+++ vee/commands/git.py
@@ -7,17 +7,22 @@ from vee.utils import makedirs
 
 @command(
     argument('-r', '--repo', help='which repo to use'),
+    argument('--stree', action='store_true', help='launch SourceTree'),
     help='run a git command in a repository',
     parse_known_args=True,
 )
 def git(args, *command):
 
+    home = args.assert_home()
+    repo = home.get_env_repo(args.repo)
+
+    if args.stree:
+        call(['stree', repo.work_tree])
+        return
+
     if not command:
         print style_error('please provide a git command')
         return 1
     
-    home = args.assert_home()
-    repo = home.get_env_repo(args.repo)
-
     makedirs(repo.work_tree)
     repo.git(*command, silent=True)
