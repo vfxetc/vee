319a4631fad5548cd9f45ee11169e08fcfa3107a
Mike Boers <westernx@mikeboers.com>
2015-03-16 14:44:53 -0700

    Add `vee sqlite` for quickly opening the database

---
+++ vee/commands/sqlite3.py
@@ -0,0 +1,18 @@
+import os
+
+from vee.cli import style_error
+from vee.commands.main import command, argument
+from vee.subproc import call
+from vee.utils import makedirs
+
+
+@command(
+    help='run a command in the database',
+    parse_known_args=True,
+    aliases=['sqlite'],
+)
+def sqlite3(args, *command):
+    home = args.assert_home()
+    cmd = ['sqlite3', home.db.path]
+    cmd.extend(command)
+    os.execvp('sqlite3', cmd)
