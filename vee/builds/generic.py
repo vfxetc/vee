0b4a9dda33efde1c59f63fa1c478827cad93666f
Mike Boers <westernx@mikeboers.com>
2015-03-19 11:55:31 -0700

    vee-develop.sh is called with vee_develop_setenv callback

--- vee/builds/generic.py
+++ vee/builds/generic.py
@@ -4,6 +4,8 @@ import shutil
 from vee.cli import style_note, style
 from vee.envvars import join_env_path
 from vee import log
+from vee.utils import find_in_tree
+from vee.subproc import call, bash_source
 
 
 class GenericBuild(object):
@@ -34,9 +36,22 @@ class GenericBuild(object):
     def develop(self):
         pkg = self.package
 
-        for name in ('bin', 'scripts'):
-            path = os.path.join(pkg.build_path, name)
-            if os.path.exists(path):
-                log.info(style_note("Adding ./%s to $PATH" % name))
-                pkg.environ['PATH'] = join_env_path('./' + name, pkg.environ.get('PATH', '@'))
+        dev_sh = find_in_tree(pkg.build_path, 'vee-develop.sh')
+        if dev_sh:
+
+            log.info(style_note('Found vee-develop.sh'))
+
+            def setenv(name, value):
+                log.info('vee develop setenv %s "%s"' % (name, value))
+                pkg.environ[name] = value
+
+            with log.indent():
+                bash_source(os.path.basename(dev_sh), callbacks=dict(vee_develop_setenv=setenv), cwd=os.path.dirname(dev_sh))
+
+        else:
+            for name in ('bin', 'scripts'):
+                path = os.path.join(pkg.build_path, name)
+                if os.path.exists(path):
+                    log.info(style_note("Adding ./%s to $PATH" % name))
+                    pkg.environ['PATH'] = join_env_path('./' + name, pkg.environ.get('PATH', '@'))
 
