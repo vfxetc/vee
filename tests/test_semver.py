60e37a40d642bc3362f4b3c865602e1622e997f3
Mike Boers <westernx@mikeboers.com>
2015-03-25 11:08:53 -0700

    Make a semantic Version and VersionExpr class

---
+++ tests/test_semver.py
@@ -0,0 +1,243 @@
+from . import *
+
+from vee.semver import Version, VersionExpr
+
+
+class TestVersions(TestCase):
+
+    def test_release(self):
+
+        v = Version('1.0.0')
+        self.assertEqual(v.release, (1, 0, 0))
+        self.assertEqual(str(v), '1.0.0')
+
+        v = Version('0.5')
+        self.assertEqual(v.release, (0, 5))
+        self.assertEqual(str(v), '0.5')
+
+    def test_pep_epoch(self):
+
+        v = Version('1.0.0')
+        self.assertEqual(v.epoch, 0)
+        self.assertEqual(str(v), '1.0.0')
+
+        v = Version('123!1.0.0')
+        self.assertEqual(v.epoch, 123)
+        self.assertEqual(str(v), '123!1.0.0')
+
+    def test_pep_pre_release(self):
+
+        v = Version('0.5b1')
+        self.assertEqual(v.release, (0, 5))
+        self.assertEqual(v.pep_pre_release, ('b', 1))
+        self.assertEqual(str(v), '0.5b1')
+        
+        v = Version('0.5beta1')
+        self.assertEqual(v.release, (0, 5))
+        self.assertEqual(v.pep_pre_release, ('beta', 1))
+        self.assertEqual(str(v), '0.5beta1')
+
+        v = Version('0.5rc6')
+        self.assertEqual(v.release, (0, 5))
+        self.assertEqual(v.pep_pre_release, ('rc', 6))
+        self.assertEqual(str(v), '0.5rc6')
+
+    def test_pep_post_release(self):
+
+        v = Version('1.2.3.post5')
+        self.assertEqual(v.release, (1, 2, 3))
+        self.assertEqual(v.post_release, ('post', 5))
+        self.assertEqual(str(v), '1.2.3.post5')
+
+    def test_pep_dev_release(self):
+
+        v = Version('0.5.dev2')
+        self.assertEqual(v.release, (0, 5))
+        self.assertEqual(v.dev_release, ('dev', 2))
+        self.assertEqual(str(v), '0.5.dev2')
+
+    def test_sem_pre_release(self):
+
+        v = Version('0.5-alpha')
+        self.assertEqual(v.release, (0, 5))
+        self.assertEqual(v.sem_pre_release, ('alpha', ))
+        self.assertEqual(str(v), '0.5-alpha')
+
+        v = Version('0.5-alpha.1')
+        self.assertEqual(v.release, (0, 5))
+        self.assertEqual(v.sem_pre_release, ('alpha', 1))
+        self.assertEqual(str(v), '0.5-alpha.1')
+
+    def test_build_metadata(self):
+
+        v = Version('0.5-alpha+abcdef')
+        self.assertEqual(v.release, (0, 5))
+        self.assertEqual(v.build_metadata, ('abcdef', ))
+        self.assertEqual(str(v), '0.5-alpha+abcdef')
+
+        v = Version('0.5-alpha+abcdef.20150325')
+        self.assertEqual(v.release, (0, 5))
+        self.assertEqual(v.build_metadata, ('abcdef', 20150325))
+        self.assertEqual(str(v), '0.5-alpha+abcdef.20150325')
+
+    def test_unknown(self):
+
+        v = Version('0.5@unknown')
+        self.assertEqual(v.release, (0, 5))
+        self.assertEqual(v.unknown, ('@unknown', ))
+
+    def test_git_rev(self):
+
+        v = Version('abcdef')
+        self.assertEqual(v.release, None)
+        self.assertEqual(v.unknown, ('abcdef', ))
+
+    def test_comparisons(self):
+
+        a = Version('1.0.0')
+        b = Version('1.0.0')
+
+        self.assertTrue(a == b)
+        self.assertFalse(a != b)
+
+        a = Version('1.0')
+        b = Version('2.0')
+
+        self.assertTrue(a != b)
+        self.assertTrue(a <= b)
+        self.assertTrue(a < b)
+        self.assertFalse(a == b)
+        self.assertFalse(a >= b)
+        self.assertFalse(a > b)
+
+        a = Version('1.0.0a')
+        b = Version('1.0.0')
+
+        self.assertTrue(a != b)
+        self.assertTrue(a <= b)
+        self.assertTrue(a < b)
+        self.assertFalse(a == b)
+        self.assertFalse(a >= b)
+        self.assertFalse(a > b)
+
+    def test_ordering(self):
+
+        vs = [Version(x) for x in '''
+
+            2.1.1
+            1.0.0
+            2.0.0
+            2.1.0
+            0.5.9
+            0.5
+            1
+            2
+
+            1.0.0a
+            1.0.0a1
+            1.0.0b
+            1.0.0rc4
+            1.0.0rc1
+            1.0.0.dev1
+            1.0.0.dev2
+            1.0.0.post1
+
+            1.0.0-alpha
+            1.0.0-alpha.1
+            1.0.0-alpha.beta
+            1.0.0-beta
+            1.0.0-beta.2
+            1.0.0-beta.11
+            1.0.0-rc.1
+
+            1.0.0@unknown
+
+        '''.strip().split()]
+        vs = [str(v) for v in sorted(vs)]
+
+        self.assertEqual(vs, '''
+            0.5
+            0.5.9
+            1
+            1.0.0a0
+            1.0.0a1
+            1.0.0b0
+            1.0.0rc1
+            1.0.0rc4
+            1.0.0.dev1
+            1.0.0.dev2
+            1.0.0-alpha
+            1.0.0-alpha.1
+            1.0.0-alpha.beta
+            1.0.0-beta
+            1.0.0-beta.2
+            1.0.0-beta.11
+            1.0.0-rc.1
+            1.0.0@unknown
+            1.0.0
+            1.0.0.post1
+            2
+            2.0.0
+            2.1.0
+            2.1.1
+        '''.strip().split(), '\n'.join(vs))
+
+    def test_eq_expr(self):
+        expr = VersionExpr('== 1.0.0')
+        self.assertTrue(expr.eval('1.0.0'))
+        self.assertFalse(expr.eval('1.0.0a1'))
+        self.assertFalse(expr.eval('2'))
+
+    def test_ne_expr(self):
+        expr = VersionExpr('!= 1.0.0')
+        self.assertFalse(expr.eval('1.0.0'))
+        self.assertTrue(expr.eval('1.0.0a1'))
+        self.assertTrue(expr.eval('2'))
+
+    def test_gt_expr(self):
+        expr = VersionExpr('> 1.0.0')
+        self.assertFalse(expr.eval('1.0.0'))
+        self.assertFalse(expr.eval('1.0.0a1'))
+        self.assertTrue(expr.eval('2'))
+
+    def test_gte_expr(self):
+        expr = VersionExpr('>= 1.0.0')
+        self.assertTrue(expr.eval('1.0.0'))
+        self.assertFalse(expr.eval('1.0.0a1'))
+        self.assertTrue(expr.eval('2'))
+
+    def test_lt_expr(self):
+        expr = VersionExpr('< 1.0.0')
+        self.assertFalse(expr.eval('1.0.0'))
+        self.assertTrue(expr.eval('1.0.0a1'))
+        self.assertFalse(expr.eval('2'))
+
+    def test_lte_expr(self):
+        expr = VersionExpr('<= 1.0.0')
+        self.assertTrue(expr.eval('1.0.0'))
+        self.assertTrue(expr.eval('1.0.0a1'))
+        self.assertFalse(expr.eval('2'))
+
+    def test_compatibility_expr(self):
+
+        expr = VersionExpr('~= 1.0.0')
+        self.assertTrue(expr.eval('1.0.0'))
+        self.assertTrue(expr.eval('1.0.1'))
+        self.assertFalse(expr.eval('1.0.0a1'))
+        self.assertFalse(expr.eval('2.0.0'))
+
+
+    def test_multiple_exprs(self):
+
+        expr = VersionExpr('>= 1.0.0, < 2.0.0')
+        self.assertTrue(expr.eval('1.0.0'))
+        self.assertTrue(expr.eval('1.0.1'))
+        self.assertFalse(expr.eval('2.0.0'))
+        self.assertFalse(expr.eval('1.0.0a'))
+        self.assertFalse(expr.eval('0.9.0'))
+
+
+
+
+
+
---
+++ vee/semver.py
@@ -0,0 +1,238 @@
+import copy
+import functools
+import re
+
+
+class _Return(Exception):
+    pass
+
+def _gt(a, b, presence_is_greater=None):
+    if a is not None and b is not None:
+        if a > b:
+            raise _Return(True)
+        if a < b:
+            raise _Return(False)
+    elif presence_is_greater is not None:
+        if a is not None:
+            raise _Return(presence_is_greater)
+        elif b is not None:
+            raise _Return(not presence_is_greater)
+
+
+@functools.total_ordering
+class Version(object):
+    """A blend bettween `SemVer 2.0`_ and `PEP 440`_.
+
+    .. _"SemVer 2.0": http://semver.org/
+    .. _"PEP 440": https://www.python.org/dev/peps/pep-0440
+
+    """
+
+    def __init__(self, raw):
+
+        # Epoch (PEP)
+        m = re.match(r'^(\d+)!(.+)$', raw)
+        if m:
+            self.epoch = int(m.group(1))
+            raw = m.group(2)
+        else:
+            self.epoch = 0
+
+        # Release (PEP and SemVER)
+        m = re.match(r'^(\d+(?:\.\d+)*)', raw)
+        if m:
+            self.release = tuple(int(x) for x in m.group(1).split('.'))
+            raw = raw[m.end(1):] # Leave the separator.
+        else:
+            self.release = None
+
+        # Pre-Release (PEP)
+        m = re.match(r'^(a(?:lpha)?|b(?:eta)?|rc)(\d*)(\.|\+|-|$)', raw)
+        if m:
+            self.pep_pre_release = (m.group(1), int(m.group(2) or 0))
+            raw = raw[m.end(2):]
+        else:
+            self.pep_pre_release = None
+
+        # Post-Release (PEP)
+        m = re.match(r'^\.(p(?:ost)?)(\d+)(\.|\+|-|$)', raw)
+        if m:
+            self.post_release = (m.group(1), int(m.group(2) or 0))
+            raw = raw[m.end(2):]
+        else:
+            self.post_release = None
+
+        # Dev-Release (PEP)
+        m = re.match(r'^\.(d(?:ev)?)(\d+)(\.|\+|-|$)', raw)
+        if m:
+            self.dev_release = (m.group(1), int(m.group(2) or 0))
+            raw = raw[m.end(2):]
+        else:
+            self.dev_release = None
+
+        # Pre-Release (SemVer)
+        m = re.match(r'-([\w-]+(\.[\w-]+)*)(\+|$)', raw) # SemVer forbids underscore, but meh.
+        if m:
+            self.sem_pre_release = tuple(int(x) if x.isdigit() else x for x in m.group(1).split('.'))
+            raw = raw[m.end(1):]
+        else:
+            self.sem_pre_release = None
+
+        # Build metadata (PEP and SemVer, but PEP calls it "local version")
+        m = re.match(r'^\+([\w-]+(\.[\w-]+)*)$', raw)
+        if m:
+            self.build_metadata = tuple(int(x) if x.isdigit() else x for x in m.group(1).split('.'))
+            raw = raw[m.end(1):]
+        else:
+            self.build_metadata = None
+
+        self.unknown = tuple(int(x) if x.isdigit() else x for x in raw.split('.')) if raw else None
+
+    @property
+    def local_version(self):
+        return self.build_metadata
+
+    @property
+    def pre_release(self):
+        return self.pep_pre_release or self.sem_pre_release
+
+    def __str__(self):
+        chunks = []
+        if self.epoch:
+            chunks.append('%d!' % self.epoch)
+        if self.release:
+            chunks.append('.'.join(str(x) for x in self.release))
+        if self.pep_pre_release:
+            chunks.append('%s%s' % self.pep_pre_release)
+        if self.post_release:
+            chunks.append('.%s%s' % self.post_release)
+        if self.dev_release:
+            chunks.append('.%s%s' % self.dev_release)
+        if self.sem_pre_release:
+            chunks.append('-' + '.'.join(str(x) for x in self.sem_pre_release))
+        if self.build_metadata:
+            chunks.append('+' + '.'.join(str(x) for x in self.build_metadata))
+        if self.unknown:
+            chunks.append('.'.join(str(x) for x in self.unknown))
+        return ''.join(chunks)
+
+    def __repr__(self):
+        return '%s(%r)' % (self.__class__.__name__, str(self))
+
+    def __eq__(a, b):
+
+        if not isinstance(a, Version):
+            a = Version(a)
+        if not isinstance(b, Version):
+            b = Version(b)
+
+        return a.__dict__ == b.__dict__
+
+    def __ne__(a, b):
+        return not (a == b)
+
+    def __gt__(a, b):
+
+        # Where A is greater than B if A comes after B in the development
+        # cycle.
+        
+        if not isinstance(a, Version):
+            a = Version(a)
+        if not isinstance(b, Version):
+            b = Version(b)
+
+        try:
+            _gt(a.epoch, b.epoch)
+            _gt(a.release, b.release)
+            _gt(a.pep_pre_release, b.pep_pre_release, presence_is_greater=False)
+            _gt(a.post_release, b.post_release, presence_is_greater=True)
+            _gt(a.dev_release, b.dev_release, presence_is_greater=False)
+            _gt(a.sem_pre_release, b.sem_pre_release, presence_is_greater=False)
+            _gt(a.unknown, b.unknown, presence_is_greater=False)
+        except _Return as r:
+            return r.args[0]
+
+        return False
+
+
+_expr_ops = {}
+
+def _op(name):
+    def decorator(func):
+        _expr_ops[name] = func
+        return func
+    return decorator
+
+
+@_op('==')
+def _op_eq(a, b):
+    return a == b
+@_op('!=')
+def _op_ne(a, b):
+    return a != b
+
+@_op('<=')
+def _op_gte(a, b):
+    return a <= b
+@_op('>=')
+def _op_lte(a, b):
+    return a >= b
+@_op('<')
+def _op_gt(a, b):
+    return a < b
+@_op('>')
+def _op_lt(a, b):
+    return a > b
+
+@_op('===')
+def _op_arbitrary_equality(a, b):
+    # This is supposed to be a string comparison, but we don't have that.
+    return a == b
+
+@_op('~=')
+def _op_compatible_release(a, b):
+
+    # This is supposed to match symantic compatibility.
+
+    # If it is too early, then it isn't compatible.
+    if a < b:
+        return False
+
+    # Make sure it isn't very far in the future.
+    c = Version('1')
+    release = list(b.release)
+    release[-1] = 0
+    release[-2] += 1
+    c.release = tuple(release)
+
+    if a > c:
+        return False
+
+    return True
+
+
+
+class VersionExpr(object):
+
+    def __init__(self, raw):
+        self.clauses = []
+        for chunk in re.split(r'\s*,\s*', raw.strip()):
+            m = re.match(r'^(%s)\s*(\S+)$' % '|'.join(re.escape(x) for x in _expr_ops), chunk)
+            if not m:
+                raise ValueError('could not parse version expr chunk: %r' % chunk)
+            op, raw_version = m.groups()
+            version = Version(raw_version)
+            self.clauses.append((op, version))
+
+    def eval(self, v):
+        if not isinstance(v, Version):
+            v = Version(v)
+        for op, op_v in self.clauses:
+            print '%s %s %s' % (v, op, op_v)
+            op_func = _expr_ops[op]
+            if not op_func(v, op_v):
+                return False
+        return True
+
+
+
