14c7821c05095b43b8328ff17ad59571c22e4c8c
Mike Boers <westernx@mikeboers.com>
2015-04-21 18:16:04 -0700

    `vee gc` no longer breaks foreign keys

--- vee/commands/gc.py
+++ vee/commands/gc.py
@@ -6,6 +6,13 @@ from vee.cli import style_note
 from vee import log
 
 
+def delete_package(con, id_):
+    con.execute('DELETE FROM package_dependencies WHERE depender_id = ? or dependee_id = ?', [id_, id_])
+    con.execute('DELETE FROM links WHERE package_id = ?', [id_])
+    con.execute('DELETE FROM shared_libraries WHERE package_id = ?', [id_])
+    con.execute('DELETE FROM packages WHERE id = ?', [id_])
+
+
 @command(
     argument('-n', '--dry-run', action='store_true'),
 )
@@ -14,28 +21,30 @@ def gc(args):
     home = args.assert_home()
     con = home.db.connect()
 
-    print style_note('Cleaning installed packages')
-    package_ids = []
-    install_paths_to_id = {}
-    for row in con.execute('SELECT id, name, install_path from packages ORDER by created_at DESC'):
+    with con:
 
-        id_, name, install_path = row
-        log.debug('%s %s %s' % (id_, name, install_path))
+        print style_note('Cleaning installed packages')
+        package_ids = []
+        install_paths_to_id = {}
+        for row in con.execute('SELECT id, name, install_path from packages ORDER by created_at DESC'):
 
-        if not os.path.exists(install_path):
-            print '%s no longer exists at %s; deleting' % (name, install_path)
-            if not args.dry_run:
-                con.execute('DELETE FROM packages WHERE id = ?', [id_])
+            id_, name, install_path = row
+            log.debug('%s %s %s' % (id_, name, install_path))
+
+            if not os.path.exists(install_path):
+                print '%s no longer exists at %s; deleting' % (name, install_path)
+                if not args.dry_run:
+                    delete_package(con, id_)
+                    continue
+
+            real_id = install_paths_to_id.get(install_path)
+            if real_id:
+                print '%s %d is a duplicate of %s; deleting' % (name, id_, real_id)
+                # TODO: update any links or package_dependencies which to point to this.
+                if not args.dry_run:
+                    delete_package(con, id_)
                 continue
-
-        real_id = install_paths_to_id.get(install_path)
-        if real_id:
-            print '%s %d is a duplicate of %s; deleting' % (name, id_, real_id)
-            # TODO: update any links or package_dependencies which to point to this.
-            if not args.dry_run:
-                con.execute('DELETE FROM packages WHERE id = ?', [id_])
-            continue
-        install_paths_to_id[install_path] = id_
+            install_paths_to_id[install_path] = id_
 
 
 
