82edde2c1d8d5dea523c58343f4fee2e0317adfc
Mike Boers <github@mikeboers.com>
2017-09-21 13:54:35 -0400

    Avoid errors for doctor if there is no git.

--- vee/__about__.py
+++ vee/__about__.py
@@ -1,5 +1,10 @@
 import os
 import subprocess
 
-__version__ = '0.1-dev'
-__revision__ = subprocess.check_output(['git', 'describe', '--tags', '--always', '--dirty'], cwd=os.path.dirname(__file__)).strip()
+__version__ = '0.2.0'
+
+try:
+    __revision__ = subprocess.check_output(['git', 'describe', '--tags', '--always', '--dirty'], cwd=os.path.dirname(__file__)).strip()
+except subprocess.CalledProcessError:
+    __revision__ = 'unknown'
+
