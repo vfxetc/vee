0a0a9ebeb46753957b111aa6bafdb09639c5f100
Mike Boers <mikeb@markmedia.co>
2017-10-24 12:04:42 -0400

    Doctor allows more recent versions of vendored packages.

--- vee/commands/doctor.py
+++ vee/commands/doctor.py
@@ -3,10 +3,23 @@ import sys
 
 import setuptools
 import virtualenv
+import packaging
+import wheel
 
 from vee.cli import style, style_note, style_warning
 from vee.commands.main import command, argument
 from vee.subproc import call, which
+from vee._bootstrap import vendor_prefix
+
+
+def parse_version(x):
+    parts = x.split('.')
+    for i, x in enumerate(parts):
+        try:
+            parts[i] = int(x)
+        except ValueError:
+            pass
+    return tuple(parts)
 
 
 @command(
@@ -51,12 +64,17 @@ def doctor(args):
                 print style_error('cannot find %s' % name)
                 return 1
 
-    print style_note('==> dependencies')
-    for name, expected_version, in [('setuptools', '18.0.1'), ('virtualenv', '13.1.2')]:
+    print style_note('==> vendored dependencies')
+    for name, expected_version, in [
+        ('setuptools', '36.0.0'),
+        ('virtualenv', '15.0.0'),
+        ('packaging', '16.0'),
+        ('wheel', '0.29.0'),
+    ]:
         module = globals()[name]
         actual_version = module.__version__
-        if expected_version == actual_version:
-            print style_note(name + ':', expected_version)
+        if parse_version(expected_version) <= parse_version(actual_version):
+            print style_note(name + ':', actual_version)
         else:
             print style('%s: %s (expected vendored %s) from %s' % (name, actual_version, expected_version, module.__file__), 'yellow')
             res = 2
@@ -72,7 +90,8 @@ def doctor(args):
     print style_note('==> configuration')
     home = args.assert_home()
     print style_note('home:', home.root)
-
+    print style_note('vendor:', vendor_prefix)
+    
     try:
         repo = home.get_env_repo()
     except ValueError:
