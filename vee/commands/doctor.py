c656b7114fb36f9eed65a008ccfd92734d61bdf3
Mike Boers <git@mikeboers.com>
2020-07-26 14:06:32 -0400

    Clean up `vee doctor` output

--- vee/commands/doctor.py
+++ vee/commands/doctor.py
@@ -1,11 +1,6 @@
 import os
 import sys
 
-import setuptools
-import virtualenv
-import packaging
-import wheel
-
 from vee.cli import style, style_note, style_warning
 from vee.commands.main import command, argument
 from vee.subproc import call, which
@@ -45,37 +40,42 @@ def doctor(args):
         print(about.__revision__)
         return
 
-    print(style_note('==> VEE'))
-    print(style_note('version:', about.__version__))
-    print(style_note('revision:', about.__revision__))
-    print(style_note('package:', os.path.abspath(os.path.join(__file__, '..', '..'))))
+    print(style_note('==> core'))
+    print(style_note('version:', repr(about.__version__)))
+    print(style_note('revision:', repr(about.__revision__)))
+    print(style_note('package:', repr(os.path.abspath(os.path.join(__file__, '..', '..')))))
 
     res = 0
 
-    def find_command(name, warn=False):
+    def find_command(name):
         path = which(name)
         if path:
-            print(style_note('%s:' % name, path))
+            print(style_note(f"{name}:", repr(path)))
+            return 0
         else:
-            if warn:
-                print(style_warning('cannot find %s' % name))
-            else:
-                print(style_error('cannot find %s' % name))
-                return 1
+            print(style(f"{name}:", 'yellow'), None)
+            return 1
 
-    print(style_note('==> vendored dependencies'))
+    print(style_note('==> dependencies'))
     for name, expected_version, in [
         ('setuptools', '36.0.0'),
         ('virtualenv', '15.0.0'),
         ('packaging', '16.0'),
         ('wheel', '0.29.0'),
     ]:
-        module = globals()[name]
+
+        try:
+            module = __import__(name)
+        except ImportError as e:
+            print(style(f"{name}:", 'yellow'), None)
+            res = 2
+            continue
+
         actual_version = module.__version__
         if parse_version(expected_version) <= parse_version(actual_version):
-            print(style_note(name + ':', actual_version))
+            print(style_note(f"{name}:", f"{actual_version!r} >= {expected_version!r}"))
         else:
-            print(style('%s: %s (expected vendored %s) from %s' % (name, actual_version, expected_version, module.__file__), 'yellow'))
+            print(style(f"{name}:", 'yellow'), f"{actual_version!r} < {expected_version!r} (from {module.__file__})")
             res = 2
 
     print(style_note('==> executables'))
@@ -84,28 +84,28 @@ def doctor(args):
     if sys.platform == 'darwin':
         res = find_command('install_name_tool') or res
     if sys.platform.startswith('linux'):
-        res = find_command('patchelf', warn=True) or res
+        res = find_command('patchelf') or res
 
-    print(style_note('==> Python runtime'))
-    print(style_note('sys.real_prefix:', getattr(sys, 'real_prefix', '<none>')))
-    print(style_note('sys.base_prefix:', getattr(sys, 'base_prefix', '<none>')))
-    print(style_note('sys.prefix:', sys.prefix))
+    print(style_note('==> runtime'))
+    print(style_note('sys.real_prefix:', repr(getattr(sys, 'real_prefix', None))))
+    print(style_note('sys.base_prefix:', repr(getattr(sys, 'base_prefix', None))))
+    print(style_note('sys.prefix:', repr(sys.prefix)))
 
-    print(style_note('==> configuration'))
+    print(style_note('==> config'))
     home = args.assert_home()
-    print(style_note('home:', home.root))
+    print(style_note('home:', repr(home.root)))
     try:
         repo = home.get_env_repo()
+        print(style_note('repo:', repr(repo.name), repr(repo.remote_url)))
     except ValueError:
-        print(style_warning('no default repo.', 'Use `vee repo add --default URL`.'))
-        return
-    print(style_note('repo:', repo.name, repo.remote_url))
+        print(style("repo:", 'yellow'), None)
+        res = res or 3
 
     print(style_note('==> summary'))
     if not res:
-        print(style('Everything looks OK', 'green'))
+        print(style('Everything looks OK.', 'green'))
     else:
-        print(style('Something may be wrong', 'yellow'))
+        print(style('Something looks wrong.', 'yellow'))
 
     return res
 
