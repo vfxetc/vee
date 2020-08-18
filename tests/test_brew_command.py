858f306f1d8f088178cbd729a3874cda1d375f82
Mike Boers <git@mikeboers.com>
2020-08-18 13:47:47 -0400

    Deactivate homebrew tests, and remove installer test
    We're not going to focus on Homebrew for a while, and the installer has been removed, so...

--- tests/test_brew_command.py
+++ tests/test_brew_command.py
@@ -5,11 +5,15 @@ class TestHomebrew(TestCase):
 
     def test_brew_1_install(self):
 
+        return skip()
+        
         home = self.class_home()
         home.main(['brew', 'install', 'sqlite'])
 
     def test_brew_2_link(self):
 
+        return skip()
+        
         home = self.class_home()
         home.main(['link', '-e', 'test', 'homebrew+sqlite'])
 
